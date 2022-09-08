e56361664ceec11000ef7f37b6ab22197a12d8b9
apoorva1509 <f20190597@goa.bits-pilani.ac.in>
2022-09-08 14:06:08 +0530

    fix data_not_loaded_error

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -112,6 +112,7 @@ WEBDRIVER_BASEURL = "http://superset:8088/"
 WEBDRIVER_BASEURL_USER_FRIENDLY = WEBDRIVER_BASEURL
 
 SQLLAB_CTAS_NO_LIMIT = True
+ENABLE_PROXY_FIX = True
 
 #
 # Optionally import superset_config_docker.py (which will have been included on
