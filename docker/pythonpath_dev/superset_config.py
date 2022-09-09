8425ed01400805835f8ac9691657c4471b1bb668
apoorva1509 <f20190597@goa.bits-pilani.ac.in>
2022-09-09 18:41:34 +0530

    Slack integration

--- docker/pythonpath_dev/superset_config.py
+++ docker/pythonpath_dev/superset_config.py
@@ -103,8 +103,26 @@ class CeleryConfig(object):
 
 CELERY_CONFIG = CeleryConfig
 
+SLACK_API_TOKEN = "xoxb-2397059419024-3311832884097-xPb0Q7eoU0S8rt5KmZgENLUS"
+
+SCREENSHOT_LOCATE_WAIT = 100
+SCREENSHOT_LOAD_WAIT = 600
+
 FEATURE_FLAGS = {"ALERT_REPORTS": True}
 ALERT_REPORTS_NOTIFICATION_DRY_RUN = True
+
+WEBDRIVER_TYPE = "chrome"
+WEBDRIVER_OPTION_ARGS = [
+    "--force-device-scale-factor=2.0",
+    "--high-dpi-support=2.0",
+    "--headless",
+    "--disable-gpu",
+    "--disable-dev-shm-usage",
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-extensions",
+]
+
 WEBDRIVER_BASEURL = "http://superset:8088/"
 # The base URL for the email report hyperlinks.
 WEBDRIVER_BASEURL_USER_FRIENDLY = WEBDRIVER_BASEURL
