70bb3a87205ef90dabdd800dfcca6492fe313b2c
apoorva1509 <f20190597@goa.bits-pilani.ac.in>
2022-09-12 18:46:45 +0530

    Install chrome driver

--- docker/docker-bootstrap.sh
+++ docker/docker-bootstrap.sh
@@ -49,19 +49,3 @@ elif [[ "${1}" == "app-gunicorn" ]]; then
   /usr/bin/run-server.sh
 fi
 
-CHROMEDRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE` && \
-mkdir -p /opt/chromedriver-$CHROMEDRIVER_VERSION && \
-curl -sS -o /tmp/chromedriver_linux64.zip http://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip && \
-unzip -qq /tmp/chromedriver_linux64.zip -d /opt/chromedriver-$CHROMEDRIVER_VERSION && \
-rm /tmp/chromedriver_linux64.zip && \
-chmod +x /opt/chromedriver-$CHROMEDRIVER_VERSION/chromedriver && \
-ln -fs /opt/chromedriver-$CHROMEDRIVER_VERSION/chromedriver /usr/local/bin/chromedriver
-
-
-curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
-echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list && \
-apt-get -yqq update && \
-apt-get -yqq install google-chrome-stable && \
-rm -rf /var/lib/apt/lists/*
-
-
