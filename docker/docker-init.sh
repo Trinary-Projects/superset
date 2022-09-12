e6e6c630059aeeb25edcaa067d34b28ced440a09
apoorva1509 <f20190597@goa.bits-pilani.ac.in>
2022-09-12 19:26:03 +0530

    fixes-chrome driver

--- docker/docker-init.sh
+++ docker/docker-init.sh
@@ -79,18 +79,18 @@ fi
 
 echo_step "5" "Starting" "Installing chrome driver"
 CHROMEDRIVER_VERSION=`curl -sS chromedriver.storage.googleapis.com/LATEST_RELEASE` && \
-mkdir -p /opt/chromedriver-$CHROMEDRIVER_VERSION && \
-curl -sS -o /tmp/chromedriver_linux64.zip http://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip && \
-unzip -qq /tmp/chromedriver_linux64.zip -d /opt/chromedriver-$CHROMEDRIVER_VERSION && \
-rm /tmp/chromedriver_linux64.zip && \
-chmod +x /opt/chromedriver-$CHROMEDRIVER_VERSION/chromedriver && \
-ln -fs /opt/chromedriver-$CHROMEDRIVER_VERSION/chromedriver /usr/local/bin/chromedriver
+    mkdir -p /opt/chromedriver-$CHROMEDRIVER_VERSION && \
+    curl -sS -o /tmp/chromedriver_linux64.zip http://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip && \
+    unzip -qq /tmp/chromedriver_linux64.zip -d /opt/chromedriver-$CHROMEDRIVER_VERSION && \
+    rm /tmp/chromedriver_linux64.zip && \
+    chmod +x /opt/chromedriver-$CHROMEDRIVER_VERSION/chromedriver && \
+    ln -fs /opt/chromedriver-$CHROMEDRIVER_VERSION/chromedriver /usr/local/bin/chromedriver
 echo_step "5" "Complete" "Installed chrome driver"
 
 echo_step "6" "Starting" "Initializing driver"
 curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
-echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list && \
-apt-get -yqq update && \
-apt-get -yqq install google-chrome-stable && \
-rm -rf /var/lib/apt/lists/*
+    echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list && \
+    apt-get -yqq update && \
+    apt-get -yqq install google-chrome-stable && \
+    rm -rf /var/lib/apt/lists/*
 echo_step "6" "Complete" "Completed"
